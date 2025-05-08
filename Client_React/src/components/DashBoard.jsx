2bc492b418b98d7b43367e44cfb27050203cd374
Krishna Alaspure <krishnaalas@gmail.com>
2025-05-09 00:58:32 +0530

    chat bot UI

--- Client_React/src/components/DashBoard.jsx
+++ Client_React/src/components/DashBoard.jsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import Chatbot from "./ChatBot/Chatbot";
 
 
 const DashBoard = () => {
@@ -12,6 +13,7 @@ const DashBoard = () => {
       <div className='flex flex-col items-center md:text-2xl text-lg text-white  font-bold justify-center  rounded-lg p-10'>
       <h1>Working on Something Big! </h1>
       <button className='border-2 p-2 rounded-lg m-4 bg-gradient-to-r from-sky-400 to-sky-900 ' onClick={onClickHandle}>See the Portfolio</button>
+      <Chatbot/>
       </div>
     </div>
   )
